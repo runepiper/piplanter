4f4d2cca0506131f4275624b50034eeafbc5e74c
Rune Piper <rune@visuellverstehen.de>
2017-01-12 08:49:44 +0100

    [TASK] Add initialization for GPIO pins

--- classes/Kickstart/Base/AppController.php
+++ classes/Kickstart/Base/AppController.php
@@ -6,13 +6,38 @@ class AppController
 {
     public function __construct()
     {
-        $this->loadRouter();
+        $this->initializeRouter();
+        $this->initializeGPIOPins();
     }
 
-    private function loadRouter()
+    private function initializeRouter()
     {
         $router = new Router();
         require_once __DIR__.'/../../../config/routes.php';
         $router->execute();
+    }
+
+    private function initializeGPIOPins()
+    {
+        // Waterpump
+        if (file_exists('/sys/class/gpio/gpio17') === false) {
+            file_put_contents('/sys/class/gpio/export', 17);
+            sleep(1);
+            file_put_contents('/sys/class/gpio/gpio17/direction', 'out');
+        }
+
+        // Moisture sensor
+        if (file_exists('/sys/class/gpio/gpio27') === false) {
+            file_put_contents('/sys/class/gpio/export', 27);
+            sleep(1);
+            file_put_contents('/sys/class/gpio/gpio27/direction', 'in');
+        }
+
+        // Water level
+        if (file_exists('/sys/class/gpio/gpio22') === false) {
+            file_put_contents('/sys/class/gpio/export', 22);
+            sleep(1);
+            file_put_contents('/sys/class/gpio/gpio22/direction', 'in');
+        }
     }
 }
